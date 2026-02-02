7990c7239749c0197e123f3ec71fe44287957d9c
ant9012 <antd39487@gmail.com>
2026-02-01 21:40:48 -0500

    Update detours.h

--- RSDKv5/detours.h
+++ RSDKv5/detours.h
@@ -95,8 +95,8 @@
 //
 
 #if (_MSC_VER < 1299)
-typedef LONG LONG_PTR;
-typedef ULONG ULONG_PTR;
+// typedef LONG LONG_PTR;
+// typedef ULONG ULONG_PTR;
 #endif
 
 ///////////////////////////////////////////////// SAL 2.0 Annotations w/o SAL.
